8d6b806966629a50e10f8ef4f62e06ac014431e5
Philip Resech <philipresech@gmail.com>
2023-12-03 21:47:35 +0100

    Solve Day3 y2023 Part 2

--- src/main/kotlin/y2023/Day3.kt
+++ src/main/kotlin/y2023/Day3.kt
@@ -71,9 +71,85 @@ class Day3 {
             line.fold(0) { acc, (_, current) -> acc + current }
         }.sum()
     }
+    data class Field(
+        val row: Int,
+        val index: Int
+    )
+
+    data class Neighbor(
+        val gear: Field,
+        val position: Field
+    )
+
+    data class GearNumber(
+        val startPosition: Field,
+        val number: Int,
+    )
+
+    fun solveSecond(): Int {
+        val gears = input.flatMapIndexed { rowNr, line ->
+            line.mapIndexed { index, char ->
+                if (char == '*') {
+                    Field(rowNr, index)
+                } else null
+            }.filterNotNull()
+        }
+
+        val neighbors = gears.flatMap { (row, index) ->
+            val fields: MutableList<Neighbor> = mutableListOf()
+
+            if (row > 0) {
+                if (input[row - 1][index - 1].isDigit()) fields.add(Neighbor(Field(row, index), Field(row - 1, index - 1)))
+                if (input[row - 1][index].isDigit()) fields.add(Neighbor(Field(row, index), Field(row - 1, index)))
+                if (input[row - 1][index + 1].isDigit()) fields.add(Neighbor(Field(row, index), Field(row - 1, index + 1)))
+            }
+
+            if (input[row][index - 1].isDigit()) fields.add(Neighbor(Field(row, index), Field(row, index - 1)))
+            if (input[row][index + 1].isDigit()) fields.add(Neighbor(Field(row, index), Field(row, index + 1)))
+
+            if (row < input.size - 1) {
+                if (input[row + 1][index - 1].isDigit()) fields.add(Neighbor(Field(row, index), Field(row + 1, index - 1)))
+                if (input[row + 1][index].isDigit()) fields.add(Neighbor(Field(row, index), Field(row + 1, index)))
+                if (input[row + 1][index + 1].isDigit()) fields.add(Neighbor(Field(row, index), Field(row + 1, index + 1)))
+            }
+
+            fields
+        }.groupBy( {it.gear}, {it.position})
+
+        return neighbors.map { (gear, neighbors) ->
+            val numbers = neighbors.map {(row, index) ->
+                var start = index
+                var end = index
+
+                while (start >= 0 && input[row][start].isDigit()) {
+                    start -= 1
+                }
+                while (input[row].length > end && input[row][end].isDigit()) {
+                    end += 1
+                }
+
+                var test = ""
+
+                for (pos in ((start + 1)..<end)) {
+                    test += input[row][pos]
+                }
+
+                GearNumber(Field(row, start), test.toInt())
+            }.distinct()
+
+            Pair(gear, numbers)
+        }.toMap()
+            .filter { (_, numbers) -> numbers.size == 2 }
+            .map { (_, numbers) ->
+                numbers.fold(1) { acc, current -> acc * current.number }
+            }
+            .sum()
+    }
 
 }
 
 fun main() {
     println("First Result: ${Day3().solveFirst()}")
+
+    println("Second Result: ${Day3().solveSecond()}")
 }
